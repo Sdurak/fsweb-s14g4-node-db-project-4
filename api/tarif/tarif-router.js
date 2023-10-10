bdfcbbadfbf106636faf1d51b128bff3818917b3
Sdurak <sdurak91@hotmail.com>
2023-10-10 15:35:18 +0300

    "Proje S14G4"

---
+++ api/tarif/tarif-router.js
@@ -0,0 +1,12 @@
+const router = require("express").Router();
+const mw = require("./tarif-middleware");
+
+router.get("/:id", mw.checkTarifId, (req, res, next) => {
+  try {
+    res.json(req.existTarif);
+  } catch (error) {
+    next(error);
+  }
+});
+
+module.exports = router;
